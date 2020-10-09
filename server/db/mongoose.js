f7424455f8aefcf3a590ed7027eceb3e84793764
BernardoMB <bmondragonbrozon@gmail.com>
2020-10-08 19:41:24 -0500

    Secured database connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,5 +2,6 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const HOST = 'localhost:27017';
 const DATABASE = 'TodoApp';
-mongoose.connect(`mongodb://nodejs-todo-app:rYDpM62XQ4orlDlLOst8aNOK655jm578o75Slt4E4EQtckMkeRKfw6hAz1M5AzJIMFYkXiaPLIJHow5fYz2IBg%3D%3D@nodejs-todo-app.mongo.cosmos.azure.com:10255/?ssl=true&appName=@nodejs-todo-app@`);
+const connectionString = process.env.MONGODB_URI || `mongodb://${HOST}/${DATABASE}`;
+mongoose.connect(connectionString);
 module.exports = {mongoose};
